0378addec474780940200d739ad26860463f317e
adrianbenitovalverde <adrianbenitovalverde@gmail.com>
2023-09-20 15:49:22 +0200

    minor changes

---
+++ app/funnel/page.js
@@ -0,0 +1,8 @@
+export default function funnel ({}) {
+
+    return (
+        <div>
+            <h1>Funnel</h1>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import {Container} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import Link from "next/link";
 
 export default function Home() {
   return (
    <Container>
      <Grid2 container>
          <Grid2><span>Grid item</span></Grid2>
+         <Grid2 item key={"linkPorp"}>
+             <Link href={"/properties"}>ir a propiedades</Link>
+         </Grid2>
+         <Grid2 item key={"linkfunnel"}>
+             <Link href={"/funnel"}>ir a funnel</Link>
+         </Grid2>
      </Grid2>
    </Container>
   )
