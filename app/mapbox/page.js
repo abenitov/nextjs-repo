ad95cb63f1060622744b66148467990556599819
adrianbenitovalverde <adrianbenitovalverde@gmail.com>
2023-09-22 17:55:50 +0200

    minor changes

--- app/mapbox/page.js
+++ app/mapbox/page.js
@@ -1,10 +1,12 @@
 "use client"
 import mapboxgl from '!mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css'
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import {useEffect, useRef, useState} from "react";
 import {Container, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2"; // eslint-disable-line import/no-webpack-loader-syntax
 import barrios from "../../public/Barrios de Barcelona.json";
+import axios from "axios";
 
 export default function Mapbox() {
 
@@ -19,8 +21,61 @@ export default function Mapbox() {
     const [selectedZoneCoordinates, setSelectedZoneCoordinates] = useState(null);
     const [selectedMapToDisplay, setSelectedMapToDisplay] = useState("1"); // 1 or 2
 
+
+
+    const getAllProperties = () => {
+
+        const ALGOLIA_APP_ID = 'VUOWCCZNT1';
+        const ALGOLIA_API_KEY = '40f1304cc89701e5055bde13fc0d4aa8';
+        const INDEX_NAME = 'dev-es-properties'; // Reemplaza con el nombre de tu índice en Algolia
+
+// URL de la API de Algolia
+        const algoliaURL = `https://${ALGOLIA_APP_ID}-dsn.algolia.net/1/indexes/${INDEX_NAME}/query`;
+
+// Configura Axios con tu clave de API
+        const axiosInstance = axios.create({
+            baseURL: algoliaURL,
+            headers: {
+                'X-Algolia-API-Key': ALGOLIA_API_KEY,
+                'X-Algolia-Application-Id': ALGOLIA_APP_ID,
+            },
+        });
+
+        axiosInstance.post('', {
+            params: 'query=barcelona',
+        }).then(
+            (response) => {
+                console.log(response.data.hits);
+                if(response.data.hits != undefined && response.data.hits.length > 0){
+
+                    //Iterate over hits and get location.lattitude and location.longitude to add them to the map
+                    response.data.hits.forEach(function (hit,index) {
+                        console.log(hit.location.longitude + " " + hit.location.latitude)
+                      new mapboxgl.Marker()
+                            .setLngLat([hit.location.longitude, hit.location.latitude]).setPopup(new mapboxgl.Popup()
+                          .setHTML(hit.description))
+                            .addTo(map2.current);
+                        new mapboxgl.Marker()
+                            .setLngLat([hit.location.longitude, hit.location.latitude]).setPopup(new mapboxgl.Popup()
+                            .setHTML(hit.description))
+                            .addTo(map.current);
+                    })
+
+                }
+            }
+        ).catch(
+            (error) => {
+                console.log(error);
+            }
+        );
+        //Fetch all properties from algolia and return them
+
+
+    }
     console.log("mapboxInit")
     useEffect(() => {
+
+
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
@@ -47,9 +102,12 @@ export default function Mapbox() {
         });
         map2.current.addControl(draw);
 
-        map2.current.on('draw.create', (e) => { });
-        map2.current.on('draw.delete', (e) => { });
-        map2.current.on('draw.update', (e) => { });
+        map2.current.on('draw.create',  (e) => {   var x=draw.getAll();
+            setSelectedZoneCoordinates(x['features'][0]['geometry']['coordinates'][0]) })
+        map2.current.on('draw.delete', (e) => {   var x=draw.getAll();
+            setSelectedZoneCoordinates(x['features'][0]['geometry']['coordinates'][0]) })
+        map2.current.on('draw.update', (e) => {   var x=draw.getAll();
+            setSelectedZoneCoordinates(x['features'][0]['geometry']['coordinates'][0]) })
 
         map.current.on('load', () => {
             //Iterate over barrios and add them to the map
@@ -95,8 +153,12 @@ export default function Mapbox() {
                 });
 
             });
+
+
+            getAllProperties();
         });
     });
+
     return (
         <Container>
             <Grid2 container>
@@ -118,23 +180,24 @@ export default function Mapbox() {
                         </Select>
                     </FormControl>
                 </Grid2>
-                { selectedZoneCoordinates !== null ?
-                <Grid2 item xs={12} key={"coor"}>
-                    <h1>Coordinates</h1>
-                    <p>{JSON.stringify(selectedZoneCoordinates)}</p>
-                </Grid2> : null}
-                <Grid2 xs={12} md={12} item key={"linkfunnel"} display={selectedMapToDisplay === "1" ? "block": "none"}>
+
+                <Grid2 xs={12} md={12} item key={"linkfunnel"} sx={selectedMapToDisplay === "1" ? { visibility: "visible", position: "relative", top: "0" } :{ visibility: "hidden", position: "absolute", top: "-10000" }}>
                     <div>
-                        <div ref={mapContainer} className="map-container"/>
+                        <div  ref={mapContainer} className="map-container"/>
                     </div>
                 </Grid2>
-                <Grid2 xs={12} md={12} item key={"linkfunnel2"} display={selectedMapToDisplay === "2" ? "block": "none"}>
+                <Grid2 xs={12} md={12} item key={"linkfunnel2"} sx={selectedMapToDisplay === "2" ? { visibility: "visible", position: "relative", top: "0" } :{ visibility: "hidden", position: "absolute", top: "-10000" }}>
                     <div>
-                        <div ref={mapContainer2} className="map-container"/>
+                        <div  ref={mapContainer2} className="map-container"/>
                     </div>
                 </Grid2>
                 <Grid2 xs={12} md={12} item key={"linkfunnel3"}>
                 </Grid2>
+                { selectedZoneCoordinates !== null ?
+                    <Grid2 item xs={12} key={"coor"}>
+                        <h1>Coordenadas para filtrar resultados:</h1>
+                        <p style={{fontSize:"0.5rem"}}>{JSON.stringify(selectedZoneCoordinates)}</p>
+                    </Grid2> : null}
             </Grid2>
 
         </Container>
