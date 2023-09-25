3dc4c738e066ff3aa5a057a3d79bd8a298272da2
adrianbenitovalverde <adrianbenitovalverde@gmail.com>
2023-09-25 17:21:59 +0200

    minor changes

--- app/components/map.js
+++ app/components/map.js
@@ -13,7 +13,6 @@ export default function Map({allProperties,getPropertiesByCoordenates}) {
 
     const isMobile = useMediaQuery('(max-width:600px)');
     const [showMap, setShowMap] = useState(!window.matchMedia('(max-width:600px)').matches);
-    const [selectedMapToDisplay, setSelectedMapToDisplay] = useState("1"); // 1 or 2
 
     console.log("mapboxInit")
 
@@ -33,42 +32,19 @@ export default function Map({allProperties,getPropertiesByCoordenates}) {
                         {showMap ? "Ocultar mapa" : "Mostrar mapa"}
                     </Button>: null
                     }
-                    {showMap ?
-                        <FormControl>
-                            <InputLabel id="select-label">Tipo de mapa</InputLabel>
-                            <Select
-                                labelId="select-label"
-                                id="select"
-                                value={selectedMapToDisplay}
-                                onChange={(e) =>{
-                                    console.log(e.target.value)
-                                    setSelectedMapToDisplay(e.target.value)
-                                }}
-                            >
-                                <MenuItem value={'1'}>Mapa 1</MenuItem>
-                                <MenuItem value={'2'}>Mapa 2</MenuItem>
-                            </Select>
-
-                        </FormControl> : null}
                 </Grid2>
 
 
 
-                <Grid2 xs={12} md={12} item key={"linkfunnel"} sx={showMap && selectedMapToDisplay === "1" ? {
+                <Grid2 xs={12} md={12} item key={"linkfunnel"} sx={showMap ? {
                     visibility: "visible",
                     position: "relative",
                     top: "0"
                 } : {visibility: "hidden", position: "absolute", top: "-10000"}}>
-                    <Mapbox version={1} allProperties={allProperties} getPropertiesByCoordenates={getPropertiesByCoordenates}/>
+                    <Mapbox allProperties={allProperties} getPropertiesByCoordenates={getPropertiesByCoordenates}/>
                 </Grid2>
 
-                <Grid2 xs={12} md={12} item key={"linkfunnel2"} sx={showMap && selectedMapToDisplay === "2" ? {
-                    visibility: "visible",
-                    position: "relative",
-                    top: "0"
-                } : {visibility: "hidden", position: "absolute", top: "-10000"}}>
-                    <Mapbox version={2} allProperties={allProperties} getPropertiesByCoordenates={getPropertiesByCoordenates}/>
-                </Grid2>
+
 
 
         </Grid2>
--- app/components/mapbox.js
+++ app/components/mapbox.js
@@ -3,11 +3,11 @@ import mapboxgl from '!mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css'
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import {useEffect, useRef, useState} from "react";
-import {Button, Container, FormControl, InputLabel, MenuItem, Select, useMediaQuery} from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2"; // eslint-disable-line import/no-webpack-loader-syntax
+
 import barrios from "../../public/Barrios de Barcelona.json";
-import axios from "axios";
-import PropertyList from "@/app/components/PropertyList";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import {Button} from "@mui/material";
+
 
 export default function Mapbox({version, allProperties, getPropertiesByCoordenates}) {
 
@@ -25,146 +25,147 @@ export default function Mapbox({version, allProperties, getPropertiesByCoordenat
 
         if (map.current) return; // initialize map only once
 
-        if (version === 1) {
-            map.current = new mapboxgl.Map({
-                container: mapContainer.current,
-                style: 'mapbox://styles/mapbox/streets-v12',
-                center: [lng, lat],
-                zoom: zoom
-            });
-
-            map.current.on('load', () => {
-                //Iterate over barrios and add them to the map
-                barrios.features.forEach(function (feature, index) {
-                    map.current.addSource("zone" + index, {
-                        'type': 'geojson',
-                        'data': {
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'Polygon',
-                                'coordinates': feature.coordinates
-                            }
-
-                        }
-                    });
-                    map.current.addLayer({
-                        'id': "zone" + index,
-                        'type': 'fill',
-                        'source': "zone" + index,
-                        'layout': {},
-                        'paint': {
-                            'fill-opacity': 0.3,
-                            'fill-color': '#00e3ff',
-                        }
-                    });
-                    map.current.on('click', "zone" + index, (e) => {
-                        console.log("click")
-                        //Copy map.current.getSource("zone" + index)._data.geometry.coordinates[0] to coordinates
-                        var coordinates = [...map.current.getSource("zone" + index)._data.geometry.coordinates[0]]
-                        console.log(coordinates)
-                        getPropertiesByCoordenates(coordinates)
-                    });
-
-                    map.current.on('mouseenter', "zone" + index, () => {
-                        map.current.getCanvas().style.cursor = 'pointer';
-                    });
-                    map.current.addLayer({
-                        'id': "zone" + index + '-outline',
-                        'type': 'line',
-                        'source': "zone" + index,
-                        'layout': {},
-                        'paint': {
-                            'line-color': '#172ab3',
-                            'line-width': 1
-                        }
-                    });
-
-                });
+            initMap();
 
 
-                if (allProperties != undefined && allProperties.length > 0) {
-
-                    //Iterate over hits and get location.lattitude and location.longitude to add them to the map
-                    allProperties.forEach(function (hit, index) {
-                        console.log(hit.location.longitude + " " + hit.location.latitude)
-                        new mapboxgl.Marker()
-                            .setLngLat([hit.location.longitude, hit.location.latitude]).setPopup(new mapboxgl.Popup()
-                            .setHTML(hit.description))
-                            .addTo(map.current);
-                    })
-
-                }
-            });
-        } else {
-            map.current = new mapboxgl.Map({
-                container: mapContainer.current,
-                style: 'mapbox://styles/mapbox/streets-v12',
-                center: [lng, lat],
-                zoom: zoom
-            });
 
 
-            map.current.on('load', () => {
-                //Iterate over barrios and add them to the map
-                const draw = new MapboxDraw({
-                    displayControlsDefault: false,
-// Select which mapbox-gl-draw control buttons to add to the map.
-                    controls: {
-                        polygon: true,
-                        trash: true
-                    },
-// Set mapbox-gl-draw to draw by default.
-// The user does not have to click the polygon control button first.
-                    defaultMode: 'draw_polygon'
-                });
-                map.current.addControl(draw);
 
-                map.current.on('draw.create', (e) => {
-                    var x = draw.getAll();
-                    var coordinates = x['features'][0]['geometry']['coordinates'][0]
-                    console.log(coordinates)
-                    getPropertiesByCoordenates(coordinates)
-                })
-
-                map.current.on('draw.delete', (e) => {
-                    var x = draw.getAll();
-                    var coordinates = x['features'][0]['geometry']['coordinates'][0]
-                    console.log(coordinates)
-                    getPropertiesByCoordenates(coordinates)
-                })
-
-                map.current.on('draw.update', (e) => {
-                    var x = draw.getAll();
-                    var coordinates = x['features'][0]['geometry']['coordinates'][0]
-                    console.log(coordinates)
-                    getPropertiesByCoordenates(coordinates)
-                })
-
-
-                if (allProperties != undefined && allProperties.length > 0) {
-
-                    //Iterate over hits and get location.lattitude and location.longitude to add them to the map
-                    allProperties.forEach(function (hit, index) {
-                        console.log(hit.location.longitude + " " + hit.location.latitude)
-                        new mapboxgl.Marker()
-                            .setLngLat([hit.location.longitude, hit.location.latitude]).setPopup(new mapboxgl.Popup()
-                            .setHTML(<a> ir a ver</a>))
-                            .addTo(map.current);
-                    })
-
-                }
-            });
-        }
 
 
     },[]);
 
+    const initMap = () => {
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        map.current.on('load', () => {
+            //Iterate over barrios and add them to the map
+
+            if (allProperties != undefined && allProperties.length > 0) {
+
+                //Iterate over hits and get location.lattitude and location.longitude to add them to the map
+                allProperties.forEach(function (hit, index) {
+                    console.log(hit.location.longitude + " " + hit.location.latitude)
+                    new mapboxgl.Marker()
+                        .setLngLat([hit.location.longitude, hit.location.latitude]).setPopup(new mapboxgl.Popup()
+                        .setHTML(hit.description))
+                        .addTo(map.current);
+                })
+
+            }
+        });
+    }
+
+    const displayZones = () => {
+        barrios.features.forEach(function (feature, index) {
+            map.current.addSource("zone" + index, {
+                'type': 'geojson',
+                'data': {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Polygon',
+                        'coordinates': feature.coordinates
+                    }
+
+                }
+            });
+            map.current.addLayer({
+                'id': "zone" + index,
+                'type': 'fill',
+                'source': "zone" + index,
+                'layout': {},
+                'paint': {
+                    'fill-opacity': 0.3,
+                    'fill-color': '#00e3ff',
+                }
+            });
+            map.current.on('click', "zone" + index, (e) => {
+                console.log("click")
+                //Copy map.current.getSource("zone" + index)._data.geometry.coordinates[0] to coordinates
+                var coordinates = [...map.current.getSource("zone" + index)._data.geometry.coordinates[0]]
+                console.log(coordinates)
+                getPropertiesByCoordenates(coordinates)
+            });
+
+            map.current.on('mouseenter', "zone" + index, () => {
+                map.current.getCanvas().style.cursor = 'pointer';
+            });
+            map.current.addLayer({
+                'id': "zone" + index + '-outline',
+                'type': 'line',
+                'source': "zone" + index,
+                'layout': {},
+                'paint': {
+                    'line-color': '#172ab3',
+                    'line-width': 1
+                }
+            });
+
+        });
+    }
+
+    const drawZone = () => {
+        const draw = new MapboxDraw({
+            displayControlsDefault: false,
+// Select which mapbox-gl-draw control buttons to add to the map.
+            controls: {
+                polygon: true,
+                trash: true
+            },
+// Set mapbox-gl-draw to draw by default.
+// The user does not have to click the polygon control button first.
+            defaultMode: 'draw_polygon'
+        });
+        map.current.addControl(draw);
+
+        map.current.on('draw.create', (e) => {
+            var x = draw.getAll();
+            var coordinates = x['features'][0]['geometry']['coordinates'][0]
+            console.log(coordinates)
+            getPropertiesByCoordenates(coordinates)
+        })
+
+        map.current.on('draw.delete', (e) => {
+            var x = draw.getAll();
+            var coordinates = x['features'][0]['geometry']['coordinates'][0]
+            console.log(coordinates)
+            getPropertiesByCoordenates(coordinates)
+        })
+
+        map.current.on('draw.update', (e) => {
+            var x = draw.getAll();
+            var coordinates = x['features'][0]['geometry']['coordinates'][0]
+            console.log(coordinates)
+            getPropertiesByCoordenates(coordinates)
+        })
+
+    }
+
+    const resetMap = () => {
+        map.current.remove()
+        initMap()
+
+    }
 
     return (
+                <Grid2 container xs={12}>
+                    <Grid2 item key={"button1"}>
+                        <Button onClick={displayZones}>Buscar por zone</Button>
+                        <Button onClick={drawZone}>Dibujar zona</Button>
+                        <Button onClick={resetMap}>Limpiar mapa</Button>
+                    </Grid2>
+                    <Grid2 item key={"mapas"}  xs={12}>
+                        <div>
+                            <div ref={mapContainer} className="map-container"/>
+                        </div>
+                    </Grid2>
+                </Grid2>
 
-                <div>
-                    <div ref={mapContainer} className="map-container"/>
-                </div>
     )
 
 }
